32d037410a38d0a23d72d68cb69fed17874ed734
Stephen Yeargin <stephen.yeargin@gmail.com>
2013-10-17 05:27:41 +0000

    Initial commit

---
+++ create_virtual_host.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Apache
+###########################################################
+
+function apache_virtualhost {
+	# Configures a VirtualHost
+
+	# $1 - required - the hostname of the virtualhost to create 
+
+	if [ ! -n "$1" ]; then
+		echo "apache_virtualhost() requires the hostname as the first argument"
+		return 1;
+	fi
+
+	if [ -e "/etc/apache2/sites-available/$1" ]; then
+		echo /etc/apache2/sites-available/$1 already exists
+		return;
+	fi
+
+	mkdir -p /srv/www/$1/public_html /srv/www/$1/logs
+
+	echo "<VirtualHost *:80>" > /etc/apache2/sites-available/$1
+	echo "    ServerName $1" >> /etc/apache2/sites-available/$1
+	echo "    DocumentRoot /srv/www/$1/public_html/" >> /etc/apache2/sites-available/$1
+	echo "    ErrorLog /srv/www/$1/logs/error.log" >> /etc/apache2/sites-available/$1
+        echo "    CustomLog /srv/www/$1/logs/access.log combined" >> /etc/apache2/sites-available/$1
+	echo "</VirtualHost>" >> /etc/apache2/sites-available/$1
+
+	a2ensite $1
+
+	touch /tmp/restart-apache2
+}
+
+apache_virtualhost $1
